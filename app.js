0c0dbe6b160aa794a5d39988c47b787658922890
Honeyfield <HoneyfieK@corning.com>
2020-05-25 22:33:21 -0400

    adjusted styling so each bubble looks like a bubble, now I need to figure out how colors work

--- app.js
+++ app.js
@@ -13,13 +13,16 @@ class Bubble {
         this.randomStart()
         this.bubble = document.createElement('div')
         this.bubble.style.position = 'absolute'
-        this.bubble.style.borderRadius = `${this._size}px`
+        this.bubble.style.borderRadius = `50%`
         this.bubble.style.height = `${this._size}px`
         this.bubble.style.width = `${this._size}px`
         this.bubble.style.filter = `blur(${this._blur}px)`
         this.bubble.style.transition = '4s ease'
-        this.bubble.style.background = this._color
+        // this.bubble.style.background = this._color //need to find a method to adjust the color based on the background or user choice
+        this.bubble.style.background = 'radial-gradient(ellipse at center,  rgba(255,255,255,0.5) 0%,rgba(255,255,255,0) 70%);'
+        this.bubble.style.boxShadow = '0 20px 30px rgba(0, 0, 0, 0.2), inset 0px 10px 30px 5px rgba(255, 255, 255, 1)'
         this.bubble.style.opacity = 0
+        this.bubble.style.zIndex = -1
         this.bubble.style.transform = `translate(calc(${this.startPOS}vw - 50px), calc(100vh - 10px))`
         this.bubble.id = this.ID
 
@@ -66,19 +69,13 @@ class Bubble {
 
 }
 
-// const bub = new Bubble(20,20,10,2, 'body')
-// bub.createBubble()
-
-// setInterval(() => {
-//     bub.moveBubble()
-// }, 1000);
 
 function makeBubbles(){
     const number = 100
     let bubbly = []
     for(let i = 0; i < number; i++){
-        const size = Math.floor(Math.random() * 10)
-        const bub = new Bubble(size,2,'aqua', 'body')
+        const size = Math.floor(Math.random() * 100)
+        const bub = new Bubble(size,0,'#fff', 'body')
         bubbly.push(bub)
     }
 
